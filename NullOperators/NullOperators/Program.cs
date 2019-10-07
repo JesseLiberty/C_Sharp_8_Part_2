3048aa1b424ea77d910537336835bd9dcae21d77
Jesse Liberty <jesseliberty@gmail.com>
2019-10-07 10:42:44 -0400

    null operators

---
+++ NullOperators/NullOperators/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace NullOperators
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         var runner = new Runner();
+         runner.Run();
+      }
+   }
+
+   class Runner
+   {
+      public void Run()
+      {
+         var sb = new StringBuilder("Hello ");
+         sb.Append("world.");
+         var greeting = sb.ToString();
+         Console.WriteLine(greeting);
+
+         sb = null;
+        // var newString = sb?.ToString();
+        var thirdString = sb?.ToString() ?? "no value";
+         Console.WriteLine(thirdString);
+         MyMethod(sb);
+      }
+
+      public void MyMethod(StringBuilder sb)
+      {
+         var thirdString = sb?.ToString() ?? "no value";
+         Console.WriteLine(thirdString);
+
+      }
+   }
+}
