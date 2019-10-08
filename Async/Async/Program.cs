a8fd9d445b5fc90b0544e8e7a0ed790cf84e8e28
Jesse Liberty <jesseliberty@gmail.com>
2019-10-08 17:02:30 -0400

    async

---
+++ Async/Async/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Async
+{
+   class Program
+   {
+      static async Task Main(string[] args)
+      {
+         Console.WriteLine("1: Hello Udemy");
+            await LongFunction();
+            Console.WriteLine("3: Finish program");
+
+         Thread.Sleep(2000);
+      }
+
+      public static async Task LongFunction()
+      {
+         await Task.Delay(1000);
+         Console.WriteLine("2: Finish LongFunction");
+      }
+   }
+}
