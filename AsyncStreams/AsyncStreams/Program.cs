02068ba3e1e15dc47be0f317c92c8f54bbf04e0e
Jesse Liberty <jesseliberty@gmail.com>
2019-10-09 11:38:01 -0400

    async stream

---
+++ AsyncStreams/AsyncStreams/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AsyncStreams
+{
+   class Program
+   {
+      static async Task Main(string[] args)
+      {
+         await ShowValues();
+      }
+
+      public static async IAsyncEnumerable<int> CreateSequence()
+      {
+         for (int i = 0; i < 20; i++)
+         {
+            var random = new Random();
+            var randomNumber = random.Next(1000);
+            await Task.Delay(randomNumber);
+            yield return randomNumber;
+         }
+      }
+
+      public static async Task ShowValues()
+      {
+         await foreach (var val in CreateSequence())
+         {
+            Console.WriteLine(val);
+         }
+      }
+   }
+}
