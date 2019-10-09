b2d3e3534b4de45d9b61032f4b68d393825f7428
Jesse Liberty <jesseliberty@gmail.com>
2019-10-09 14:32:53 -0400

    Extension Methods

---
+++ ExtensionMethod/ExtensionMethod/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExtensionMethod
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         var runner = new Runner();
+         runner.Run();
+      }
+   }
+
+   class Runner
+   {
+      public void Run()
+      {
+         var myList = new List<string>
+         {
+            "Hello",
+            "Udemy",
+            "Jesse",
+            "Liberty",
+            "Rodrigo",
+            "Juarez"
+         };
+         var newList = myList.AllCaps();
+         foreach (string myString in newList)
+         {
+            Console.WriteLine(myString);
+         }
+      }
+   }
+
+   public static class StringStuff
+   {
+      public static List<string> AllCaps(this List<string> strings)
+      {
+         var tempList = new List<string>();
+         foreach (string myString in strings)
+         {
+            var newString = myString.ToUpper();
+            tempList.Add(newString);
+         }
+
+         return tempList;
+      }
+   }
+
+}
