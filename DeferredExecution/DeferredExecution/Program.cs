336f13db61b7cf1a6fa1dd2e9d8cacd1471b0cbf
Jesse Liberty <jesseliberty@gmail.com>
2019-10-10 15:02:35 -0400

    deferred execution

---
+++ DeferredExecution/DeferredExecution/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace DeferredExecution
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         var runner = new Runner();
+         runner.Run();
+      }
+   }
+
+   class Runner
+   {
+      public void Run()
+      {
+         var query = Enumerable.Range(0, 1000000)
+            .Select(x =>
+            {
+               Thread.Sleep(1000);
+               return x * x;
+            });
+
+         foreach (var num in query)
+         {
+            Console.WriteLine(num);
+         }
+      }
+   }
+}
