ebf5ae4ae4c07eee5a0f86c99d9da926fd0b5648
Jesse Liberty <jesseliberty@gmail.com>
2019-10-10 12:32:26 -0400

    Tuples and Hello Linq

---
+++ LINQ/LINQ/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Net.Sockets;
+
+namespace LINQ
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         var runner = new Runner();
+         runner.Run();
+      }
+   }
+
+   class Runner
+   {
+      public void Run()
+      {
+         var listOfNumbers = new List<int>
+         {
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+         };
+
+         //var query = from num in listOfNumbers
+         //   where num % 2 == 0
+         //   select num;
+
+         var query = listOfNumbers
+            .Where(num => num % 2 == 0)
+            .Select(num => num);
+
+         foreach (var num in query)
+         {
+            Console.WriteLine(num);
+         }
+
+         var query2 = from method in typeof(int).GetMethods()
+            orderby method.Name
+            group method by method.Name into groups
+            select new
+               { MethodName = groups.Key, MethodOverloads = groups.Count() };
+
+         foreach (var item in query2)
+         {
+            Console.WriteLine(item);
+         }
+
+      }
+   }
+}
---
+++ tuples/tuples/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading;
+
+namespace tuples
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         var runner = new Runner();
+         runner.Run();
+      }
+   }
+
+   class Runner
+   {
+      public void Run()
+      {
+         //var myTuple = SecondThroughFourth();
+         //(int square, int cube, int fourth) = SecondThroughFourth();
+         var (square, cube, fourth) = SecondThroughFourth();
+
+         //Console.WriteLine($"Square: {myTuple.squared} " +
+         //                  $"Cube: { myTuple.cubed} " +
+         //                  $"Four: {myTuple.fourth} ");
+         Console.WriteLine($"Square: {square} " +
+                           $"Cube: { cube} " +
+                           $"Four: {fourth} ");
+
+
+      }
+
+      public (int s, int c, int f) SecondThroughFourth()
+      {
+         var listOfInts = new List<int>
+         {
+            2, 4, 6, 8
+         };
+
+         var solution = (square: 0, cube: 0, fourth: 0);
+
+         foreach (var val in listOfInts)
+         {
+            solution.square += val * val;
+            solution.cube += val * val * val;
+            solution.fourth += val * val * val * val;
+         }
+
+         return solution;
+      }
+   }
+}
