11e79580ce86d5722953b115b4a49ba16117b2e9
Jesse Liberty <jesseliberty@gmail.com>
2019-10-10 20:10:14 -0400

    indices

---
+++ IndicesAndRanges/IndicesAndRanges/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace IndicesAndRanges
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         var runner = new Runner();
+         runner.Run();
+      }
+   }
+
+   class Runner
+   {
+      public void Run()
+      {
+         var words = new string[]
+         {
+            "the",
+            "quick",
+            "brown",
+            "fox",
+            "jumps",
+            "over",
+            "the",
+            "lazy",
+            "dog"
+         };
+
+         Console.WriteLine($"The first word is at index 0: {words[0]}");
+         Console.WriteLine($"The last word is one back from the end: {words[^1]}");
+
+         var quickBrownFox = words[2..4];
+         foreach (var word in quickBrownFox)
+         {
+            Console.Write($"{word} ");
+         }
+
+         Console.WriteLine();
+
+         quickBrownFox = words[2..];
+         foreach (var word in quickBrownFox)
+         {
+            Console.Write($"{word} ");
+         }
+
+         Console.WriteLine();
+
+         quickBrownFox = words[..5];
+         foreach (var word in quickBrownFox)
+         {
+            Console.Write($"{word} ");
+         }
+
+         Console.WriteLine();
+
+
+      }
+   }
+
+}
